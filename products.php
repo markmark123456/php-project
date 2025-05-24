dd9fd98c8fdcff4672190861c7ac27d8d9000642
admin <admin@mac.local>
2025-05-24 20:22:55 +0600

    show product to not authorizated users

--- products.php
+++ products.php
@@ -26,27 +26,24 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <?php endif; ?>
 
 
-    <?php if ($user): ?>
-        <div class="product-list">
-            <?php foreach ($products as $product): ?>
-                <div class="product">
-                    <h2><?= htmlspecialchars($product['title']) ?></h2>
-                    <p>Цена: <?= htmlspecialchars($product['price']) ?> ₽</p>
-                    <p><?= htmlspecialchars($product['description']) ?></p>
+    <div class="product-list">
+        <?php foreach ($products as $product): ?>
+            <div class="product">
+                <h2><?= htmlspecialchars($product['title']) ?></h2>
+                <p>Цена: <?= htmlspecialchars($product['price']) ?> ₽</p>
+                <p><?= htmlspecialchars($product['description']) ?></p>
 
-                    <form action="add_to_cart.php" method="post">
-                        <label>
-                            Кол-во:
-                            <input type="number" name="quantity" value="1" min="1">
-                        </label>
-                        <input type="hidden" name="product_id" value="<?= $product['id'] ?>">
-                        <button type="submit">Добавить в корзину</button>
-                    </form>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    <?php else: ?>
-        <p><a href="login.php">Войдите</a>, чтобы сделать заказ</p>
-    <?php endif; ?>
+                <form action="add_to_cart.php" method="post">
+                    <label>
+                        Кол-во:
+                        <input type="number" name="quantity" value="1" min="1">
+                    </label>
+                    <input type="hidden" name="product_id" value="<?= $product['id'] ?>">
+                    <button type="submit">Добавить в корзину</button>
+                </form>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
 </body>
 </html>
